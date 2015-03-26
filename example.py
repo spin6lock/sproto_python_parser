757a61db4f6a1bb21f2c4efe055cbfe3caf6da7e
spin6lock <john.37@gmail.com>
2015-03-26 11:53:01 +0800

    init commit

---
+++ example.py
@@ -0,0 +1,30 @@
+from pypeg2 import *
+class Type(Keyword):
+    grammar = Enum(K("string"), K("boolean"), K("integer"))
+
+class Number(Symbol):
+    regex = re.compile("\d+")
+
+class EntityName(Symbol):
+    regex = re.compile("\.([a-zA-Z0-9]+)")
+
+class Instruction(Namespace):
+    grammar = name(), attr("id", Number), ":", attr("typing", Type)
+
+class Function(List):
+    pass
+
+block = "{", maybe_some(Instruction), "}"
+Function.grammar = attr("name", EntityName), block
+
+f = parse("""
+        .Person { 
+            name 0 : string
+            id 1 : integer
+            female 2 : boolean
+        }
+        """, Function)
+
+print(f.name)
+for fitem in f:
+    print(fitem.name, fitem.typing, fitem.id)
