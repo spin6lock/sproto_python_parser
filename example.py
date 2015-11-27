571cf86f31d74823cf7534256d2c888789344a22
spin6lock <john.37@gmail.com>
2015-11-27 17:47:27 +0800

    增加数组识别

--- example.py
+++ example.py
@@ -2,32 +2,33 @@ from pypeg2 import *
 class Type(Keyword):
     grammar = Enum(K("string"), K("boolean"), K("integer"))
 
+class Array(Symbol):
+    regex = re.compile("\*")
+
 class Number(Symbol):
     regex = re.compile("\d+")
 
 class EntityName(Symbol):
     regex = re.compile("([a-zA-Z0-9]+)")
 
-class Instruction(Namespace):
-    grammar = name(), attr("id", Number), ":", attr("typing", Type)
+class FIELD(Namespace):
+    grammar = name(), attr("id", Number), ":", attr("is_array", optional(Array)), attr("typing", Type)
+
+class block(Namespace):
+    pass
 
 class Struct(List):
     pass
 
-block = "{", maybe_some([Instruction, Struct]), "}"
+
+block.grammar = "{", maybe_some([FIELD, Struct]), "}"
 Struct.grammar = ".", attr("name", EntityName), block
 
-f = parse("""
-        .Person { 
-            name 0 : string
-            id 1 : integer
-            female 2 : boolean
-            .Phone {
-                type 0 : integer
-                number 1 : string
-            }
-        }""", Struct)
+with open("array_type.sproto") as fin:
+    content = fin.read()
+    f = parse(content, Struct)
 
 print(f.name)
 for fitem in f:
-    print(fitem)
+    for k, v in fitem.items():
+        print(k, "is_array", bool(v.is_array))
